071faa0b0201a35f5f82a2047b630864332bfd2e
WhitePaper233 <1423634265@qq.com>
2022-03-27 20:58:29 +0800

    整活插件

---
+++ plugins/smartai/main.go
@@ -0,0 +1,23 @@
+// 用Go实现的世界上最牛逼的Ai 估值十亿
+package smartai
+
+import (
+	"strings"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+	// "github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() {
+	zero.OnMessage().
+		Handle(func(ctx *zero.Ctx) {
+			if ctx.Event.IsToMe {
+				var msg string
+				msg = ctx.Event.Message.String()
+				msg = strings.Replace(msg, "?", "!", -1);
+				msg = strings.Replace(msg, "？", "!", -1);
+				msg = strings.Replace(msg, "吗", "", -1);
+				ctx.Send(msg)
+			}
+		})
+}
