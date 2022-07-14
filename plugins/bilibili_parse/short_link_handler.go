da7d4332819af48633e7fb53fe4bdd0804926d94
WhitePaper233 <1423634265@qq.com>
2022-07-14 09:10:50 +0800

    删除调试冗余代码

--- plugins/bilibili_parse/short_link_handler.go
+++ plugins/bilibili_parse/short_link_handler.go
@@ -17,7 +17,6 @@ import (
 
 func HandleShortLink(ctx *zero.Ctx) {
 	// 如果是卡片信息, 则跳过
-	logger.Debugln(ctx.Event.RawMessage)
 	if strings.HasPrefix(
 		ctx.Event.RawMessage,
 		`[CQ:json,data={"app":"com.tencent.miniapp_01"&#44;`,
