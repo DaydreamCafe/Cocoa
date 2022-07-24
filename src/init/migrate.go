382f941d5160caae1ccf4960b74091457fcc4245
WhitePaper233 <1423634265@qq.com>
2022-07-24 21:15:11 +0800

    ✨ 新增模型

---
+++ src/init/migrate.go
@@ -0,0 +1,47 @@
+// Package init Bot初始化相关代码
+package init
+
+import (
+	logger "github.com/sirupsen/logrus"
+
+	"github.com/DaydreamCafe/Cocoa/V2/src/conn"
+	"github.com/DaydreamCafe/Cocoa/V2/src/model"
+)
+
+func init() {
+	db, err := conn.GetDB()
+	if err != nil {
+		logger.Panicln(err)
+	}
+
+	db.Exec("DROP TABLE IF EXISTS plugin;")
+	// 迁移插件元数据表
+	err = db.AutoMigrate(&model.Plugin{})
+	if err != nil {
+		logger.Panicln("数据库自动迁移失败:", err)
+	}
+
+	// 迁移全局插件管理表
+	err = db.AutoMigrate(&model.GlobalPluginManagement{})
+	if err != nil {
+		logger.Panicln("数据库自动迁移失败:", err)
+	}
+
+	// 迁移局部插件管理表
+	err = db.AutoMigrate(&model.LocalPluginManagement{})
+	if err != nil {
+		logger.Panicln("数据库自动迁移失败:", err)
+	}
+
+	// 迁移用户权限表
+	err = db.AutoMigrate(&model.UserPremissionModel{})
+	if err != nil {
+		logger.Panicln("数据库自动迁移失败:", err)
+	}
+
+	// 迁移用户封禁表
+	err = db.AutoMigrate(&model.BanedUserModel{})
+	if err != nil {
+		logger.Panicln("数据库自动迁移失败:", err)
+	}
+}
